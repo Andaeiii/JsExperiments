d839d9dddf9d3ad6bd3bf22e75f686bb20769399
Ande Caleb <andaeiii@aol.com>
2022-08-22 11:54:53 +0100

    trying implement spritesheets dealings

---
+++ CreateJS_FlappyBird/js/sfscript.js
@@ -0,0 +1,10 @@
+var stage, canvas, cw, ch;
+
+function initProj(){
+    stage = new createjs.Stage("spcanvas");
+ 
+   //canvas dimensions.. 
+    cw = stage.canvas.width;
+    ch = stage.canvas.height;
+ 
+}
