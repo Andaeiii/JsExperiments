0d14715bee86cc2d7c4743f5ed9f40683f71aa22
Ande Caleb <andaeiii@gmail.com>
2024-10-30 10:51:30 +0100

    three js graph

---
+++ dloch/index.js
@@ -0,0 +1,15 @@
+
+
+let name1 = "aminu"; 
+var name2 = "ocholi";
+
+let fullname = null
+
+function getFullname(){
+    fullname = name1 + name2; 
+    return fullname;
+}
+
+fullname = getFullname();
+
+console.log(fullname); 
